d863c60e74055420637cbfa31b35c7f2b4fa5478
yk045t0854t0845ht8045 <murilogiroldo.mercadopago@gmail.com>
2026-01-31 19:04:42 -0300

    mensagem

--- app/api/wz_AuthLogin/exchange/route.ts
+++ app/api/wz_AuthLogin/exchange/route.ts
@@ -75,7 +75,7 @@ export async function GET(req: Request) {
 
     // ✅ aqui sim salva cookie no host do dashboard
     const res = NextResponse.redirect(new URL(safeNext, url.origin));
-    setSessionCookie(res, { userId, email }, req); // ✅ passa req
+    setSessionCookie(res, { userId, email }, req.headers); // ✅ passa req
     return res;
   } catch (e: any) {
     console.error("[exchange] error:", e);
