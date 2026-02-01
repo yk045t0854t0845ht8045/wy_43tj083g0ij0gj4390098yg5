afa51dbb27a9e5e94fabfbbf3abcdf344805ec45
yk045t0854t0845ht8045 <murilogiroldo.mercadopago@gmail.com>
2026-01-31 22:03:59 -0300

    mensagem

--- app/dashboard/create-account/OnboardCreateAccountClient.tsx
+++ app/dashboard/create-account/OnboardCreateAccountClient.tsx
@@ -38,7 +38,7 @@ function SpinnerMini({ reduced }: { reduced: boolean }) {
       transition={{ duration: reduced ? 0 : 0.18 }}
     >
       <span className="relative h-5 w-5">
-        <span className="absolute inset-0 rounded-full border-2 border-black/15" />
+        <span className="absolute inset-0 rounded-full border-2 border-black/15 mt-1.5" />
         <motion.span
           className="absolute inset-0 rounded-full border-2 border-black/50 border-t-transparent"
           animate={reduced ? undefined : { rotate: 360 }}
