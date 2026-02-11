b850108e362e7b602f8cfc19e82c2885f2b92c5b
yk045t0854t0845ht8045 <murilogiroldo.mercadopago@gmail.com>
2026-02-11 20:07:32 -0300

    mensagem

--- sql/wz_users_add_contact_changed_at.sql
+++ sql/wz_users_add_contact_changed_at.sql
@@ -2,6 +2,4 @@
 -- Run in Supabase SQL Editor
 
 alter table if exists public.wz_users
-  add column if not exists email_changed_at timestamptz,
-  add column if not exists phone_changed_at timestamptz,
   add column if not exists password_changed_at timestamptz;
