8ff7fd8d4eed44f1911eec6b1c8afa1e8b9b1a56
yk045t0854t0845ht8045 <murilogiroldo.mercadopago@gmail.com>
2026-02-14 12:13:27 -0300

    mensagem

--- app/api/wz-auth/google/callback/route.ts
+++ app/api/wz-auth/google/callback/route.ts
@@ -1,2 +1,9 @@
-export { dynamic, revalidate, GET } from "@/app/api/wz_AuthLogin/google/callback/route";
+import { type NextRequest } from "next/server";
+import { GET as legacyGET } from "@/app/api/wz_AuthLogin/google/callback/route";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export async function GET(req: NextRequest) {
+  return legacyGET(req);
+}
